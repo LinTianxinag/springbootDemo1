c6c8a431763a303b565cc48336e97abc4baaaec1
LinTianxinag <1152453141@qq.com>
2018-04-17 14:31:32 +0800

    test war to deploy

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@ImportResource("classpath:application-beans.xml")
-public class DemoApplication {
+@ImportResource("classpath:DemoApplication.xml")
+public class DemoApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
